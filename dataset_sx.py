cb5a680b6027a8f7d56f2d91b28637bf5d1f8d36
Amos <506474841@qq.com>
2023-11-09 22:11:28 +0800

    加入滑窗数据集

--- dataset_sx.py
+++ dataset_sx.py
@@ -3,7 +3,7 @@ import numpy as np
 import h5py
 import torch
 from typing import Dict,List,Tuple
-dir_path="/home/amos/haitun/pycode/"#os.path.dirname(os.path.abspath())
+dir_path="/home/amos/haitun/pycode/"
 from torch.utils.data import Dataset
 from torch.utils.data import DataLoader,ConcatDataset
 class DatasetReader:
@@ -14,21 +14,34 @@ class DatasetReader:
         self.dim_num=4
         self.category_sahpe=self.dataset['sample_list'].shape
         
-    def get_trajectory(self,category_index:int,trajectory_index:int)->Tuple[np.array]:
+    def get_trajectory(self,category_index:int)->Tuple[np.array]:
         #"""返回一个[5,timestep_num]的np数组，5包括time x y v angle , 以及一个区域编码"""
         """返回一个[5,timestep_num]的np数组，5包括x y v angle"""
         assert category_index <= self.category_sahpe[-1] and category_index>=0 , "category_index out of range"
         trajectory_shape=self.dataset[self.dataset['sample_list'][0,category_index]].shape
-        assert trajectory_index <= trajectory_shape[-1] and trajectory_index>=0 , "trajectory_index out of range"
         
+        
+        res = []
+        for index in range(trajectory_shape[-1]):
+
         #n=self.dim_num-1
-        n = self.dim_num
-        trajectory_ref=self.dataset[self.dataset[self.dataset['sample_list'][0,category_index]][0,trajectory_index]][:,0]
-        data_list=[self.dataset[trajectory_ref[_]] for _ in range(n)]
-        #code_ref = self.dataset[self.dataset[self.dataset['sample_list'][0,category_index]][0,trajectory_index]][n,0]
-        rtn= np.array(data_list,dtype=np.float32).squeeze()
+            n = self.dim_num
+            trajectory_ref=self.dataset[self.dataset[self.dataset['sample_list'][0,category_index]][0,index]][:,0]
+            data_list=[self.dataset[trajectory_ref[_]] for _ in range(n)]
+            #code_ref = self.dataset[self.dataset[self.dataset['sample_list'][0,category_index]][0,trajectory_index]][n,0]
+            rtn= np.array(data_list,dtype=np.float32).squeeze()
+            rtn = np.transpose(rtn)
+            if len(rtn)<50:
+                continue
+            res.append(self.sliding_window(rtn, 50, 50))
+
+        rres = np.concatenate(res,axis=0)
+        # np.random.seed(10)
+        # np.random.shuffle(rres)
+        # rres_train = rres[0:int(len(rres)*0.7),:,:]
+        # rres_test = rres[int(len(rres)*0.7):,:,:]
         #return rtn, np.array(self.dataset[code_ref],dtype=np.int32) 
-        return rtn            
+        return rres            
 
     def get_category(self,category_index:int)->Tuple[(np.array,np.array)]:
         #"返回长度为轨迹数量的元组，每个元素是也是元组，第一个元素为轨迹array，第二个为区域编码"
@@ -59,25 +72,42 @@ class DatasetReader:
         trajectory_shape=self.dataset[self.dataset['sample_list'][0,category_index]].shape
         return trajectory_shape[-1]
     
+    def sliding_window(self,matrix, window_len, n):
+        new_shape = (1 + (matrix.shape[0] - window_len) // n, window_len, matrix.shape[1])
+        new_matrix = np.zeros(new_shape)
+        for i in range(new_shape[0]):
+            new_matrix[i] = matrix[i * n : i * n + window_len]
+        if (new_shape[0] - 1) * n + window_len < matrix.shape[0]:
+            new_matrix = np.concatenate((new_matrix, matrix[-window_len:, :][np.newaxis, :, :]), axis=0)
+        return new_matrix
 class SubDataset(Dataset):
     """针对某一个类别的dataset"""
     def __init__(self,category_index) -> None:
         super().__init__()    
         self.datareader = DatasetReader()
         self.category_index = category_index
+        self.trajectory_num = None
+        self.read_data()
+        
+    def read_data(self):
+        self.trajectory=self.datareader.get_trajectory(self.category_index)
+        self.trajectory_num = self.trajectory.shape[0]
+        
+        
+        
 
     def __getitem__(self,index)->(np.array,int):
-        trajectory_index=index
-        #(trajectory,zone_code)=self.datareader.get_trajectory(self.category_index,trajectory_index)
-        trajectory=self.datareader.get_trajectory(self.category_index,trajectory_index)
         
+        #(trajectory,zone_code)=self.datareader.get_trajectory(self.category_index,trajectory_index)
+        
+        trajectory = self.trajectory[index]
         #return (trajectory,zone_code) , self.category_index
         return trajectory , self.category_index
 
     def __len__(self):
-        return self.datareader.get_length_trajectory(self.category_index)
+        #return self.datareader.get_length_trajectory(self.category_index)
+        return self.trajectory_num
     
-      
 
 if __name__ == "__main__":
     # dataset=Dataset()
@@ -86,6 +116,13 @@ if __name__ == "__main__":
     dataset_list = [SubDataset(i) for i in range(14)]
     dataset1=ConcatDataset(datasets=dataset_list)
     mydata=DataLoader(dataset1,batch_size=1,shuffle=True)
+    for index, (x, y) in enumerate(mydata):
+        x=x
+        y=y
+        print(index)
+
+
+
     t=tuple(mydata)
     print(t[0])
     print(len(t))
