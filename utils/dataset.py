0d7c44c32157f59a28f05c82a8b7bf491f51b2a4
Amos <506474841@qq.com>
2023-12-02 01:46:01 +0800

    并行读取数据集

---
+++ utils/dataset.py
@@ -0,0 +1,35 @@
+import os
+import numpy as np
+import pandas as pd
+from multiprocessing import Pool
+
+# 定义处理函数，用于解析文件并转换为numpy数组
+def process_file(file_path):
+    
+    df = pd.read_csv(file_path, delimiter=' ', header=None)
+    df[0] = df[0].str.replace('T', '')   
+    # 解析每行数据并将其转换为numpy数组
+    # 将DataFrame转换为numpy数组
+    numpy_array = df.values[:, 1:].astype(np.float64)
+    
+    return numpy_array
+
+# 设置数据集文件夹路径
+dataset_folder = '/mnt/d/haitundata'
+
+# 遍历标签文件夹
+label_folders = [label_folder for label_folder in os.listdir(dataset_folder) if os.path.isdir(os.path.join(dataset_folder, label_folder))]
+label_folders.sort(key=lambda x:int(x.split('.')[0]))
+# 使用多进程处理每个标签文件夹下的文件
+pool = Pool()
+
+result = []
+for label_folder in label_folders:
+    label_folder_path = os.path.join(dataset_folder, label_folder)
+    files = [os.path.join(label_folder_path, file) for file in os.listdir(label_folder_path) if os.path.isfile(os.path.join(label_folder_path, file))]
+    result.append(pool.map(process_file, files))
+
+pool.close()
+pool.join()
+t =1 
+# result列表包含了所有文件的numpy数组
