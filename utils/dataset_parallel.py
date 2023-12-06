1eb72654e4f7a927ba91ef4feededa685b9779a7
Amos <506474841@qq.com>
2023-12-06 15:04:53 +0800

    dataset parallel

--- utils/dataset_parallel.py
+++ utils/dataset_parallel.py
@@ -1,6 +1,8 @@
 import os
 import numpy as np
 import pandas as pd
+from concurrent.futures import ThreadPoolExecutor
+from concurrent.futures import ProcessPoolExecutor
 
 # 定义处理函数，用于解析文件并转换为numpy数组
 def process_file(file_path):
@@ -17,19 +19,18 @@ def Readcsv(dataset_folder):
     """
     返回嵌套列表，第一层是类别，第二层是某一类的轨迹样本
     """
-    # # 设置数据集文件夹路径
-    # dataset_folder = '/mnt/d/haitundata'
-
     # 遍历标签文件夹
     label_folders = [label_folder for label_folder in os.listdir(dataset_folder) if os.path.isdir(os.path.join(dataset_folder, label_folder))]
     label_folders.sort(key=lambda x:int(x.split('.')[0]))
-    # 使用多进程处理每个标签文件夹下的文件
+    # 使用线程池处理每个标签文件夹下的文件
+    with ProcessPoolExecutor(max_workers=16) as executor:
+        result = []
+        for label_folder in label_folders:
+            label_folder_path = os.path.join(dataset_folder, label_folder)
+            files = [os.path.join(label_folder_path, file) for file in os.listdir(label_folder_path) if os.path.isfile(os.path.join(label_folder_path, file))]
+            res = list(executor.map(process_file, files))
+            result.append(res)
 
-    result = []
-    for label_folder in label_folders:
-        label_folder_path = os.path.join(dataset_folder, label_folder)
-        files = [os.path.join(label_folder_path, file) for file in os.listdir(label_folder_path) if os.path.isfile(os.path.join(label_folder_path, file))]
-        result.append(list(map(process_file, files)))
     # result列表包含了所有文件的numpy数组
 
     return result
