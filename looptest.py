133dfb5da2f287e82e987e0432ee067837528c93
Amos <506474841@qq.com>
2023-12-08 10:18:20 +0800

    looptest

--- looptest.py
+++ looptest.py
@@ -3,5 +3,5 @@ import subprocess
 import subprocess
 
 with open("output.log", "w") as f:
-    for i in range(30):
-        subprocess.Popen(["python", "basic_cls.py"], stdout=f)
+    for i in range(8):
+        subprocess.Popen(["python", "train.py"], stdout=f)
